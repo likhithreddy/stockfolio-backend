9b55f1bb462ac797a8db45f39725959fc52dffcb
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-07-21 16:34:14 -0400

    Add environment variable configuration for deployment

--- src/main/java/org/stocks/config/DatabaseConfig.java
+++ src/main/java/org/stocks/config/DatabaseConfig.java
@@ -1,24 +1,40 @@
 package org.stocks.config;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @Configuration
 public class DatabaseConfig {
 
-    public static String DB_USERNAME;
-    public static String DB_PASSWORD;
+    @Value("${DB_HOST:localhost}")
+    private String dbHost;
+
+    @Value("${DB_PORT:3306}")
+    private String dbPort;
+
+    @Value("${DB_NAME:stockportfolio}")
+    private String dbName;
+
+    @Value("${DB_USERNAME:root}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD:}")
+    private String dbPassword;
 
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         MysqlDataSource ds = new MysqlDataSource();
-        ds.setURL("jdbc:mysql://localhost:3306/stockportfolio");
-        ds.setUser(DB_USERNAME);
-        ds.setPassword(DB_PASSWORD);
+
+        // Build URL with environment variables
+        String url = String.format("jdbc:mysql://%s:%s/%s", dbHost, dbPort, dbName);
+        ds.setURL(url);
+        ds.setUser(dbUsername);
+        ds.setPassword(dbPassword);
+
         return ds;
     }
 }
